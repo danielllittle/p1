602f3c33dcc1f0bebbb23ded9e98a52bf2687659
Daniel Little <little@fas.harvard.edu>
2014-09-18 22:37:41 -0700

    updated page links

--- index.php
+++ index.php
@@ -6,19 +6,21 @@ ul.a {list-style-type: disc;}
 <title>DWA 15 - P1</title>
 </head>
 <h1>Daniel Little</h1>
-<img src= "images/selfie.jpg"/>
-<a href="../p2">Project 2</a>
-<a href="../p3">Project 3</a>
-<a href="../p4">Project 4</a>
-<a href="http://www.github.com/danielllittle">Github Repository</a>
-<li>In 2+ sentences describe who you are and what you do.</li>
+<img src= "images/selfie.jpg"/><br><hr>
+
+<li>Project 1&nbsp;--&nbsp;<a href="http://p1.harvardalm.me">live link</a>&nbsp;--&nbsp;<a href="http://www.github.com/danielllittle/p1">Github Repository</a></li>
+<li>Project 2&nbsp;--&nbsp;<a href="http://p2.harvardalm.me"">live link</a>&nbsp;--&nbsp;<a href="http://www.github.com/danielllittle/p2">Github Repository</a></li>
+<li>Project 3&nbsp;--&nbsp;<a href="http://p3.harvardalm.me"">live link</a>&nbsp;--&nbsp;<a href="http://www.github.com/danielllittle/p3">Github Repository</a></li>
+<li>Project 4&nbsp;--&nbsp;<a href="http://p4.harvardalm.me"">live link</a>&nbsp;--&nbsp;<a href="http://www.github.com/danielllittle/p4">Github Repository</a></li>
+<hr>
+<p>In 2+ sentences describe who you are and what you do.</li>
 <p>I'm an ALM IT student concentrating in software engineering.&nbsp;&nbsp;I work in Silicon Valley/San Francisco.</p>
 <li>Why are you taking this course?
 
 <p>Initially, the favorable course ratings and reviews made me take special notice of this course.   With PHP/HTML/CSS, I'm pushing myself a bit to get outside of my comfort zone of core java development.</p>     
 
 <li>What previous experience do you have with programming?
-<p>I have a great deal of experience on server-side application devlopment.   </p>
+ <p>I have a great deal of experience on server-side application devlopment.primarily with Java.   I have had very little exposure to PHP so I'm eager to learn. </p>
 
 <li>Are you working on a Mac, PC, or some other platform?</li>
 <p>All of the above.   I personally I use a mac.   When I am at work or school, I am just as comfortable working with Windows and Unix/Linux as well. 
