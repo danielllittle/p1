64d1f7ecb206c03f7ee5ca44687ec7208b6961d3
Daniel Little <little@fas.harvard.edu>
2014-09-18 21:27:03 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## Date
+
+09/14/2014
+
+##Live URL
+http://p1.harvardalm.me
+
+##Project Description
+
+##Demo Details
+No Demo required for Project #1.
+
+##Additional plgins, libraries, packages used
+None
---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+<style>
+ul.a {list-style-type: disc;}
+</style>
+<title>DWA 15 - P1</title>
+</head>
+<h1>Daniel Little</h1>
+<img src= "/images/selfie.jpg"/>
+<a href="../p2">Project 2</a>
+<a href="../p3">Project 3</a>
+<a href="../p4">Project 4</a>
+<a href="http://www.github.com/danielllittle">Github Repository</a>
+<li>In 2+ sentences describe who you are and what you do.</li>
+<p>I'm an ALM IT student concentrating in software engineering.&nbsp;&nbsp;I work in Silicon Valley/San Francisco.</p>
+<li>Why are you taking this course?
+
+<p>Initially, the favorable course ratings and reviews made me take special notice of this course.   With PHP/HTML/CSS, I'm pushing myself a bit to get outside of my comfort zone of core java development.</p>     
+
+<li>What previous experience do you have with programming?
+<p>I have a great deal of experience on server-side application devlopment.   </p>
+
+<li>Are you working on a Mac, PC, or some other platform?</li>
+<p>All of the above.   I personally I use a mac.   When I am at work or school, I am just as comfortable working with Windows and Unix/Linux as well. 
+<li>Anything else we should know?
+You can learn more about me @ <a href="https://www.linkedin.com/pub/daniel-little/58/468/943">Linked In</a>
+</html>
+
