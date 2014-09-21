88f3bfce5dd8afd5b23550759b206a24f5006a59
Daniel Little <little@fas.harvard.edu>
2014-09-21 01:19:53 -0700

    cleaned up HTML

--- index.php
+++ index.php
@@ -1,30 +1,32 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
 <head>
-<style>
+<style type="text/css">
 ul.a {list-style-type: disc;}
 </style>
 <title>DWA 15 - P1</title>
 </head>
 <h1>Daniel Little</h1>
-<img src= "images/selfie.jpg"/><br><hr>
-
+<div><img src= "images/selfie.jpg" alt="selfie picture"><br><hr></div>
+<ul>
 <li>Project 1&nbsp;--&nbsp;<a href="http://p1.harvardalm.me">live link</a>&nbsp;--&nbsp;<a href="http://www.github.com/danielllittle/p1">Github Repository</a></li>
-<li>Project 2&nbsp;--&nbsp;<a href="http://p2.harvardalm.me"">live link</a>&nbsp;--&nbsp;<a href="http://www.github.com/danielllittle/p2">Github Repository</a></li>
-<li>Project 3&nbsp;--&nbsp;<a href="http://p3.harvardalm.me"">live link</a>&nbsp;--&nbsp;<a href="http://www.github.com/danielllittle/p3">Github Repository</a></li>
-<li>Project 4&nbsp;--&nbsp;<a href="http://p4.harvardalm.me"">live link</a>&nbsp;--&nbsp;<a href="http://www.github.com/danielllittle/p4">Github Repository</a></li>
+<li>Project 2&nbsp;--&nbsp;<a href="http://p2.harvardalm.me">live link</a>&nbsp;--&nbsp;<a href="http://www.github.com/danielllittle/p2">Github Repository</a></li>
+<li>Project 3&nbsp;--&nbsp;<a href="http://p3.harvardalm.me">live link</a>&nbsp;--&nbsp;<a href="http://www.github.com/danielllittle/p3">Github Repository</a></li>
+<li>Project 4&nbsp;--&nbsp;<a href="http://p4.harvardalm.me">live link</a>&nbsp;--&nbsp;<a href="http://www.github.com/danielllittle/p4">Github Repository</a></li>
+</ul>
 <hr>
-<p>In 2+ sentences describe who you are and what you do.</li>
+<p>In 2+ sentences describe who you are and what you do.
 <p>I'm an ALM IT student concentrating in software engineering.&nbsp;&nbsp;I work in Silicon Valley/San Francisco.</p>
-<li>Why are you taking this course?
+<p>Why are you taking this course?</p>
 
 <p>Initially, the favorable course ratings and reviews made me take special notice of this course.   With PHP/HTML/CSS, I'm pushing myself a bit to get outside of my comfort zone of core java development.</p>     
 
-<li>What previous experience do you have with programming?
+<p>What previous experience do you have with programming?</p>
  <p>I have a great deal of experience on server-side application devlopment.primarily with Java.   I have had very little exposure to PHP so I'm eager to learn. </p>
 
-<li>Are you working on a Mac, PC, or some other platform?</li>
-<p>All of the above.   I personally I use a mac.   When I am at work or school, I am just as comfortable working with Windows and Unix/Linux as well. 
-<li>Anything else we should know?
-You can learn more about me @ <a href="https://www.linkedin.com/pub/daniel-little/58/468/943">Linked In</a>
+<p>Are you working on a Mac, PC, or some other platform?
+All of the above.   I personally I use a mac.   When I am at work or school, I am just as comfortable working with Windows and Unix/Linux as well. </p>
+<p>Anything else we should know?
+You can learn more about me @ <a href="https://www.linkedin.com/pub/daniel-little/58/468/943">Linked In</a></p>
 </html>
 
